2f699140c8d198db756a199aec32c5955b27bb25
Panji Anugrah <an.tubagusp@gmail.com>
2025-10-30 01:30:38 +0700

    new model

--- app/src/main/java/com/sslythrrr/galeri/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.sslythrrr.galeri
-
-import android.os.Environment
-
-object Constants {
-    val TARGET_DIRECTORY = "${Environment.getExternalStorageDirectory().path}/DCIM/tes/dataset_galeri/"
-}
